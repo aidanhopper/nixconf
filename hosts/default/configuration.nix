bb166c40fb9448a08e42345f40add02e8cc56c18
Aidan Hopper <aidanhop1@gmail.com>
2024-06-19 09:59:31 -0700

    1718816371

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -54,7 +54,7 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
   #services.xserver.desktopManager.gnome.enable = true;
   programs.sway = {
     enable = true;
