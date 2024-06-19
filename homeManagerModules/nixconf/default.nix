f88a68948d2976cd6beee220006a62a7b97a293a
Aidan Hopper <aidanhop1@gmail.com>
2024-06-19 08:13:57 -0700

    1718810037

--- homeManagerModules/nixconf/default.nix
+++ homeManagerModules/nixconf/default.nix
@@ -6,7 +6,7 @@ let
     ${pkgs.git}/bin/git add /nixconf
     ${pkgs.git}/bin/git commit -m $(date "+%s")
     ${pkgs.git}/bin/git push
-    nixos-rebuild $1 --flake /nixconf#$2
+    sudo nixos-rebuild $1 --flake /nixconf#$2
   '';
 
 in
