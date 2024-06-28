b661b59d3482991fca1e61aedb6e6f4a18a612c2
Aidan Hopper <aidanhop1@gmail.com>
2024-06-28 08:51:37 -0700

    1719589897

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -197,7 +197,7 @@
   systemd.services.vpn = {
     enable = true;
     serviceConfig = {
-      Type = "forking";
+      Type = "oneshot";
       ExecStart = "tailscale set --exit-node=us-sea-wg-102.mullvad.ts.net --exit-node-allow-lan-access";
       ExecStop = "tailscale set --exit-node=";
       Restart = "on-failure";
