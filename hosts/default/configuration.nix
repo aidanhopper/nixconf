ff5e0bb6102052d12157b16f50472682b98949fe
Aidan Hopper <aidanhop1@gmail.com>
2024-06-30 00:21:14 -0700

    1719732074

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -259,7 +259,7 @@
     powerManagement.enable = true;
     open = false;
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
 
