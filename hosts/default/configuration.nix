124648141d23ea34bca56ce0bf1ac368a456cee3
Aidan Hopper <aidanhop1@gmail.com>
2024-07-20 23:23:17 -0700

    1721542997

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -341,7 +341,15 @@
   };
 
   services.pufferpanel = {
-    enable = true;
+   services.pufferpanel = {
+      enable = true;
+      extraPackages = with pkgs; [ bash curl gawk gnutar gzip ];
+      package = pkgs.buildFHSEnv {
+        name = "pufferpanel-fhs";
+        runScript = lib.getExe pkgs.pufferpanel;
+        targetPkgs = pkgs': with pkgs'; [ icu openssl zlib ];
+      };
+    };
   };
 
   hardware.graphics = {
