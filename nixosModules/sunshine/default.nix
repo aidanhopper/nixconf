6669cf08b364f94248e9f785e665b4a4c6c1d557
Aidan Hopper <aidanhop1@gmail.com>
2024-07-20 18:18:20 -0700

    1721524700

--- nixosModules/sunshine/default.nix
+++ nixosModules/sunshine/default.nix
@@ -8,12 +8,9 @@
   config = lib.mkIf config.sunshine.enable {
     services.avahi.publish.enable = true;
     services.avahi.publish.userServices = true;
-    services.sunshine.enable = true;
+    services.sunshine.enable = false;
     services.sunshine.autoStart = true;
     services.sunshine.capSysAdmin = true;
-    services.sunshine.settings = {
-      file_apps = lib.mkForce "/home/aidan/.config/sunshine/apps.json";
-    };
     services.sunshine.applications.apps = with pkgs; [
       steam
     ];
