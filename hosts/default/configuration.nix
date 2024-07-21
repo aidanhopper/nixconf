f5276d3ebff85bdccc2118cb36711432a30d1955
Aidan Hopper <aidanhop1@gmail.com>
2024-07-20 18:57:45 -0700

    1721527065

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -70,6 +70,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.default = "gnome";
   services.displayManager.autoLogin.user = "aidan";
   services.displayManager.autoLogin.enable = true;
 
