e13d4dfec5411b6d60da953576a4b500ea7ba499
Aidan Hopper <aidanhop1@gmail.com>
2024-08-24 15:33:38 -0700

    1724538818

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -201,6 +201,22 @@
     ];
   };
 
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
+
  #  services.caddy = {
  #    enable = true;
  #    virtualHosts."media.aidahop.xyz".extraConfig = ''
