61a5e5e6a8d17296a68f1878fde9ae483c09c07e
Aidan Hopper <aidanhop1@gmail.com>
2024-08-31 11:08:39 -0700

    1725127719

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -66,11 +66,11 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+  services.desktopManager.gnome.enable = true;
 
-  services.xserver.displayManager = {
+  services.displayManager = {
     defaultSession = "gnome";
     autoLogin = {
       enable = true;
