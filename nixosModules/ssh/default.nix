3db2da83fd56cd7463bd52c856f8d6a96c6feb6c
Aidan Hopper <aidanhop1@gmail.com>
2024-10-01 12:34:27 -0700

    1727811267

--- nixosModules/ssh/default.nix
+++ nixosModules/ssh/default.nix
@@ -20,13 +20,6 @@
         "192.168.254.0/24"
       ];
       bantime = "24h";
-      bantime-increment = {
-        enable = true; # Enable increment of bantime after each violation
-        formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
-        multipliers = "1 2 4 8 16 32 64";
-        maxtime = "168h"; # Do not ban for more than 1 week
-        overalljails = true; # Calculate the bantime based on all the violations
-      };
     };
 
     users.users."aidan".openssh.authorizedKeys.keys = [
